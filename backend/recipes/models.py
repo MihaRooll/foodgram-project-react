70879bb9fc0f5c4f4c833d698138284d6efc7260
Mihail <katkowva@gmail.com>
2023-05-01 16:48:24 +0300

    2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from users.models import User
 from django.core.validators import MinValueValidator
 
+
 class Tag(models.Model):
     """Class to store recipe tags in the database."""
 
@@ -44,6 +45,27 @@ class Ingredient(models.Model):
         return f"{self.name} ({self.measurement_unit})"
 
 
+class RecipeIngredients(models.Model):
+    """Class to store ingredients of a particular recipe in the database."""
+
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name='recipeingredients', verbose_name='Ингредиент')
+    amount = models.PositiveIntegerField('Количество', validators=[MinValueValidator(1)])
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецептов'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['ingredient'],
+                name='unique_recipe_ingredient'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.amount} {self.ingredient}'
+
+
 class Recipe(models.Model):
     """Class to store recipes in the database."""
 
@@ -56,7 +78,7 @@ class Recipe(models.Model):
         verbose_name='Автор'
     )
     ingredients = models.ManyToManyField(
-        Ingredient,
+        RecipeIngredients,
         through='RecipeIngredients',
         related_name='recipes',
         verbose_name='Ингредиенты'
@@ -64,7 +86,9 @@ class Recipe(models.Model):
     name = models.CharField('Название', max_length=200)
     image = models.ImageField('Изображение', upload_to='recipes/')
     text = models.TextField('Описание')
-    cooking_time = models.PositiveIntegerField('Время приготовления', validators=[MinValueValidator(1)])
+    cooking_time = models.PositiveIntegerField(
+        'Время приготовления',
+        validators=[MinValueValidator(1)])
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     class Meta:
@@ -86,29 +110,6 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class RecipeIngredients(models.Model):
-    """Class to store ingredients of a particular recipe in the database."""
-
-    recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='recipeingredients', verbose_name='Рецепт')
-    ingredient = models.ForeignKey(
-        Ingredient, on_delete=models.CASCADE, related_name='recipeingredients', verbose_name='Ингредиент')
-    amount = models.PositiveIntegerField('Количество', validators=[MinValueValidator(1)])
-
-    class Meta:
-        verbose_name = 'Ингредиент рецепта'
-        verbose_name_plural = 'Ингредиенты рецептов'
-        constraints = [
-            models.UniqueConstraint(
-                fields=['recipe', 'ingredient'],
-                name='unique_recipe_ingredient'
-            )
-        ]
-
-    def __str__(self):
-        return f'{self.recipe} требует {self.amount} {self.ingredient}'
 
 
 class Favorite(models.Model):
