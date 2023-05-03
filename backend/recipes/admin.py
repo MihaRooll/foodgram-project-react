daa6039e6376a92d27c93b90a0ac82c2f11ad367
Mihail <katkowva@gmail.com>
2023-05-04 02:23:41 +0300

    41

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,7 +5,7 @@ from .models import (Favorite, Ingredient, Recipe, RecipeIngredients,
 
 
 class RecipeIngredientsInline(admin.TabularInline):
-    """Класс для отображения модели RecipeIngredients в виде встроенного блока."""
+    """Inline class for the RecipeIngredients model display."""
 
     model = RecipeIngredients
     extra = 1
@@ -13,62 +13,68 @@ class RecipeIngredientsInline(admin.TabularInline):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    """Класс для настройки отображения рецептов в панели администратора."""
+    """Class to customize recipes display in admin panel."""
 
     list_display = [
-        'pk', 'name', 'author', 'text', 'cooking_time',
-        'total_favorites', 'pub_date']
-    search_fields = ['name', 'author', 'cooking_time', 'text']
-    readonly_fields = ['total_favorites']
-    list_filter = ['name', 'pub_date', 'author', 'tags']
-    empty_value_display = '-empty-'
+        "pk",
+        "name",
+        "author",
+        "text",
+        "cooking_time",
+        "total_favorites",
+        "pub_date",
+    ]
+    search_fields = ["name", "author", "cooking_time", "text"]
+    readonly_fields = ["total_favorites"]
+    list_filter = ["name", "pub_date", "author", "tags"]
+    empty_value_display = "-empty-"
     inlines = [RecipeIngredientsInline]
 
-    @admin.display(description='Всего избранных')
+    @admin.display(description="Total favorites")
     def total_favorites(self, obj):
         return obj.favorites.count()
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    """Класс для настройки отображения тегов в панели администратора."""
+    """Class to customize tags display in admin panel."""
 
-    list_display = ['pk', 'name', 'color', 'slug']
-    search_fields = ['name', 'color', 'slug']
+    list_display = ["pk", "name", "color", "slug"]
+    search_fields = ["name", "color", "slug"]
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    """Класс для настройки отображения ингредиентов в панели администратора."""
+    """Class to customize ingredients display in admin panel."""
 
-    list_display = ['pk', 'name', 'measurement_unit']
-    search_fields = ['name', 'measurement_unit']
-    list_filter = ['name', 'measurement_unit']
+    list_display = ["pk", "name", "measurement_unit"]
+    search_fields = ["name", "measurement_unit"]
+    list_filter = ["name", "measurement_unit"]
     inlines = [RecipeIngredientsInline]
 
 
 @admin.register(RecipeIngredients)
 class RecipeIngredientsAdmin(admin.ModelAdmin):
-    """Класс для настройки отображения ингредиентов рецептов в панели администратора."""
+    """Class to customize ingredients of recipes display in admin panel."""
 
-    list_display = ['pk', 'recipe', 'ingredient', 'amount']
-    search_fields = ['recipe', 'ingredient']
-    list_filter = ['recipe', 'ingredient']
+    list_display = ["pk", "recipe", "ingredient", "amount"]
+    search_fields = ["recipe", "ingredient"]
+    list_filter = ["recipe", "ingredient"]
 
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
-    """Класс для настройки отображения избранных рецептов пользователей в панели администратора."""
+    """Class to customize users' favorite recipes display in admin panel."""
 
-    list_display = ['pk', 'user', 'recipe']
-    search_fields = ['user', 'recipe']
-    list_filter = ['user', 'recipe']
+    list_display = ["pk", "user", "recipe"]
+    search_fields = ["user", "recipe"]
+    list_filter = ["user", "recipe"]
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
-    """Класс для настройки отображения корзин покупок пользователей в панели администратора."""
+    """Class to customize users' shopping carts display in admin panel."""
 
-    list_display = ['pk', 'user', 'recipe']
-    search_fields = ['user', 'recipe']
-    list_filter = ['user', 'recipe']
+    list_display = ["pk", "user", "recipe"]
+    search_fields = ["user", "recipe"]
+    list_filter = ["user", "recipe"]
--- backend/users/models.py
+++ backend/users/models.py
@@ -5,40 +5,24 @@ from django.db import models
 class User(AbstractUser):
     """Class to store users in the database."""
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
     username = models.CharField(
-        'Username',
-        max_length=150,
-        unique=True,
-        null=False,
-        blank=False
+        "Username", max_length=150, unique=True, null=False, blank=False
     )
     email = models.EmailField(
-        'Email address',
-        max_length=254,
-        blank=False,
-        null=False,
-        unique=True
+        "Email address", max_length=254, blank=False, null=False, unique=True
     )
-    first_name = models.CharField(
-        'First name',
-        max_length=150,
-        blank=False
-    )
-    last_name = models.CharField(
-        'Last name',
-        max_length=150,
-        blank=False
-    )
+    first_name = models.CharField("First name", max_length=150, blank=False)
+    last_name = models.CharField("Last name", max_length=150, blank=False)
 
     # Удалено: password = models.CharField('Password', max_length=150)
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-        ordering = ['pk']
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["pk"]
 
     def __str__(self):
         return self.username
@@ -48,32 +32,27 @@ class Subscription(models.Model):
     """Class to store user subscriptions in the database."""
 
     user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='follower',
-        verbose_name='Follower'
+        User, on_delete=models.CASCADE,
+        related_name="follower", verbose_name="Follower"
     )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='following',
-        verbose_name='Author'
+        User, on_delete=models.CASCADE,
+        related_name="following", verbose_name="Author"
     )
 
     class Meta:
-        verbose_name = 'Subscription'
-        verbose_name_plural = 'Subscriptions'
-        ordering = ['-pk']
+        verbose_name = "Subscription"
+        verbose_name_plural = "Subscriptions"
+        ordering = ["-pk"]
         constraints = [
             models.UniqueConstraint(
-                fields=['user', 'author'],
-                name='unique_subscription'
+                fields=["user", "author"], name="unique_subscription"
             ),
             models.CheckConstraint(
-                check=~models.Q(user=models.F('author')),
-                name='no_self_subscription'
-            )
+                check=~models.Q(user=models.F("author")),
+                name="no_self_subscription"
+            ),
         ]
 
     def __str__(self):
-        return f'{self.user} is following {self.author}'
+        return f"{self.user} is following {self.author}"
