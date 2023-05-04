4efbf3bea68ab9a219606d3cd7e840785df320d5
Mihail <katkowva@gmail.com>
2023-05-04 13:30:45 +0300

    59

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,18 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (IngredientViewSet, RecipeViewSet, SelfUserView,
-                    SetPasswordRetypeView, TagViewSet, UserViewSet)
+from .views import (IngredientDisplayViewSet, RecipeManagementViewSet, CurrentUserView,
+                    ChangePasswordView, TagDisplayViewSet, SubscriptionViewSet)
 
 router = DefaultRouter()
-router.register('users', UserViewSet)
-router.register('tags', TagViewSet)
-router.register('ingredients', IngredientViewSet)
-router.register('recipes', RecipeViewSet)
+router.register('users', SubscriptionViewSet)
+router.register('tags', TagDisplayViewSet)
+router.register('ingredients', IngredientDisplayViewSet)
+router.register('recipes', RecipeManagementViewSet)
 
 urlpatterns = [
-    path('users/me/', SelfUserView.as_view()),
-    path('users/set_password/', SetPasswordRetypeView.as_view()),
+    path('users/me/', CurrentUserView.as_view()),
+    path('users/set_password/', ChangePasswordView.as_view()),
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
