155534fc356689dde5023df7b888c53a46d3e615
Mihail <katkowva@gmail.com>
2023-05-04 18:27:43 +0300

    80

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    """Custom pagination class having page_size_query_param."""
+    """Custom pagination class having page_size_query_aram."""
 
     page_size_query_param = 'limit'
