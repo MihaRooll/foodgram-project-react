4264dadc7d0e27c0fee34e48bed3c0e330b7462c
Mihail <katkowva@gmail.com>
2023-05-10 21:25:55 +0300

    145

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -196,7 +196,6 @@ class RecipeCreationSerializer(DetailedRecipeSerializer):
         ]
         RecipeIngredients.objects.bulk_create(recipe_ingredients)
 
-
     @transaction.atomic
     def create(self, validated_data):
         tags = validated_data.pop('tags')
